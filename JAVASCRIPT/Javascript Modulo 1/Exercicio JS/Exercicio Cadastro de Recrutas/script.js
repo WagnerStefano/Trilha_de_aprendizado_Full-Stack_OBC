a8e47cb15c03433c867cb15c4b1911d94b17add9
Wagner Stefano <wagnerstefano98@hotmail.com>
2023-07-19 18:56:39 -0300

    estudo dia 19/07/2023
    Calculadora - 4 operações

--- JAVASCRIPT/Javascript Modulo 1/Exercicio JS/Exercicio Cadastro de Recrutas/script.js
+++ JAVASCRIPT/Javascript Modulo 1/Exercicio JS/Exercicio Cadastro de Recrutas/script.js
@@ -4,8 +4,8 @@ const areaDeEstudo = prompt("Informe sua area de estudo")
 const anoDeNascimento = prompt("informe seu ano de nascimento")
 
 alert(
-    "Recruta cadastrado com sucesso!  " + 
-    "Nome completo: " + primeiroNome + " " + "Sobenome: " + sobrenome + 
-    "Campo de estudo: " + areaDeEstudo + 
-    "idade: " + (2023 - anoDeNascimento)
+    "Recruta cadastrado com sucesso!\n  " + 
+    "\nNome completo: " + primeiroNome + " " + "\nSobenome: " + sobrenome + 
+    "\nCampo de estudo: " + areaDeEstudo + 
+    "\nidade: " + (2023 - anoDeNascimento)
 )
