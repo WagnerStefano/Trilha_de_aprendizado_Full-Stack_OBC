2b5af69c8cba5c610c9271c26ea4586c76a181c0
Wagner Stefano <wagnerstefano98@hotmail.com>
2023-07-21 12:45:07 -0300

    estudo dos dia 20/07/2023
    exercícios condicionais em JavaScript

---
+++ JAVASCRIPT/Javascript Modulo 1/JavaScript - Logica de Programação/IF e ELSE/script.js
@@ -0,0 +1,25 @@
+
+const resultado = (6 === 6) ? "Verdadeiro" : "Falso"
+console.log(resultado)
+
+// const idade = prompt("Informe a sua idade:")
+
+// if (idade > 18){
+//     alert("Você é maior de idade")
+// }else if (idade < 12){
+//     alert("Você é menor de idade")
+// }else if ( idade < 6){
+//     alert("Você ainda é criança, retorne em alguns anos")
+// }else{
+//     alert("..")
+// }
+
+//criar uma expressão quer some o ano atual mais a idade
+//para que marque o retorno ao completar 18 anos
+// `{}`
+
+// if(6 !== 6){
+//     console.log("Verdadeiro")
+// }else{
+//     console.log("Falso")
+// }
