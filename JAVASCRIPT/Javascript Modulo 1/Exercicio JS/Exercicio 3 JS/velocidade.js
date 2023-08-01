8b9e5a37730acd51e280baa884ffd48dcffc5d91
Wagner Stefano <wagnerstefano98@hotmail.com>
2023-08-01 10:55:05 -0300

    estudo
    exercício teste de velocidade

---
+++ JAVASCRIPT/Javascript Modulo 1/Exercicio JS/Exercicio 3 JS/velocidade.js
@@ -0,0 +1,9 @@
+const veiculo1 = promtp("insira o nome do veiculo N°1:")
+const velocidade1 = prompt("insira a velocidade veiculo N° 1:")
+const veiculo2 = prompt("insira a velocidade do veiculo N° 2:")
+const velocidade2 = prompt("insira a velocidade do veiculo N°2:")
+
+const x = parseFloat(veiculo1)
+const y = parseFloat(veiculo2)
+
+alert("o veiculo")
